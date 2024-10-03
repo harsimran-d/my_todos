07c7aa0562acf5786fc0de1c39d25658b2ae3a72
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-03 22:21:27 +0530

    improve sign in form handling

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,38 +2,43 @@
 
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function () {
+  const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    const result = await signIn("credentials", {
+      username: username,
+      password: password,
+      redirect: false,
+    });
+    if (result?.ok) {
+      router.push(callbackUrl);
+    } else {
+      console.log("Sign-in failed");
+    }
+  }
   return (
     <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        onClick={async () => {
-          // write code to call hte signin funciton in
-          signIn("credentials", {
-            username: username,
-            password: password,
-            redirect: true,
-            callbackUrl: callbackUrl,
-          });
-        }}
-      >
-        Signin
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Signin</button>
+      </form>
     </div>
   );
 }
