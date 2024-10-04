9e803ee8f426420f06a67e3cd1a238d845625b1b
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-04 14:55:45 +0530

    handle input validation and show errors

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -9,8 +9,13 @@ export default function () {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  function handleInputChange() {
+    setError("");
+  }
   async function handleSubmit(e: any) {
     e.preventDefault();
     const result = await signIn("credentials", {
@@ -21,23 +26,45 @@ export default function () {
     if (result?.ok) {
       router.push(callbackUrl);
     } else {
-      console.log("Sign-in failed");
+      setError("Sign-in failed. Please try again.");
     }
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="m-auto flex w-64 flex-col space-y-2"
+      >
+        <h1 className="text-center text-2xl font-bold">Sign In</h1>
         <input
+          className="rounded-lg border-2 border-black p-2 text-xl"
           type="text"
+          required
+          placeholder="Username"
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            handleInputChange();
+          }}
         />
         <input
+          className="rounded-lg border-2 border-black p-2 text-xl"
           type="password"
+          placeholder="Password"
+          required
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            handleInputChange();
+          }}
         />
-        <button type="submit">Signin</button>
+        {error && <p className="text-red-500">{error}</p>}
+        <button
+          type="submit"
+          className="rounded-lg border-2 border-black bg-black p-2 text-xl text-white"
+        >
+          Sign In
+        </button>
       </form>
     </div>
   );
