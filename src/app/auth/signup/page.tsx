8bde0b4ea05b19fedee8036ba8c7f1b75eedab29
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-04 13:36:25 +0530

    add the client side validation with zod

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,28 +1,55 @@
 "use client";
 
 import { signUp } from "@/actions/auth/actions";
-import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { SignupSchema } from "@/zod/SignupSchema";
 
 export default function SignupPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
+  const [errorMessage, setErrorMessage] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
   const router = useRouter();
   const _signup = async () => {
-    setError("");
+    setErrorMessage({ username: "", password: "", confirmPassword: "" });
     if (password != confirmPassword) {
-      setError("Passwords do not match");
+      setErrorMessage({
+        username: "",
+        password: "",
+        confirmPassword: "Passwords do not match",
+      });
       return;
     }
-    const response = await signUp(username, password);
+    const result = SignupSchema.safeParse({
+      username,
+      password,
+    });
+
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+
+      setErrorMessage({
+        username: fieldErrors.username?.join(",") || "",
+        password: fieldErrors.password?.join(",") || "",
+        confirmPassword: "",
+      });
+      return;
+    }
+    const response = await signUp(result.data.username, result.data.password);
 
     if (response.success) {
       router.push("/auth/signin");
     } else {
-      setError(response.message);
+      setErrorMessage({
+        username: "",
+        password: "",
+        confirmPassword: response.message,
+      });
     }
   };
   return (
@@ -36,6 +63,11 @@ export default function SignupPage() {
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
       />
+      {errorMessage.username != "" ? (
+        <span className="text-red-500">{errorMessage.username}</span>
+      ) : (
+        ""
+      )}
       <input
         className="rounded-md border-2 border-black px-2 py-1 text-xl"
         type="password"
@@ -43,6 +75,11 @@ export default function SignupPage() {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      {errorMessage.password != "" ? (
+        <span className="text-red-500">{errorMessage.password}</span>
+      ) : (
+        ""
+      )}
       <input
         className="rounded-md border-2 border-black px-2 py-1 text-xl"
         type="password"
@@ -50,7 +87,11 @@ export default function SignupPage() {
         onChange={(e) => setConfirmPassword(e.target.value)}
         placeholder="Confirm Password"
       />
-      {error != "" ? <span className="text-red-500">{error}</span> : ""}
+      {errorMessage.confirmPassword != "" ? (
+        <span className="text-red-500">{errorMessage.confirmPassword}</span>
+      ) : (
+        ""
+      )}
       <button onClick={_signup} className="rounded-lg bg-black py-2 text-white">
         Signup
       </button>
