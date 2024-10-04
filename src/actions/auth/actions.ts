e618e7517f261846dbe6c06964b321eead39d161
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-04 13:36:02 +0530

    add the zod validations in the server action

--- src/actions/auth/actions.ts
+++ src/actions/auth/actions.ts
@@ -1,17 +1,29 @@
 "use server";
 import bcrypt from "bcrypt";
 import prisma from "@/lib/db";
+import { SignupSchema } from "@/zod/SignupSchema";
 
 export async function signUp(
   username: string,
   password: string,
 ): Promise<{ success: boolean; message: string }> {
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const result = SignupSchema.safeParse({
+    username,
+    password,
+  });
+
+  if (!result.success) {
+    return {
+      success: result.success,
+      message: result.error.format()._errors.toString(),
+    };
+  }
+  const hashedPassword = await bcrypt.hash(result.data.password, 10);
 
   try {
     const user = await prisma.user.create({
       data: {
-        username: username,
+        username: result.data.username,
         password: hashedPassword,
       },
     });
